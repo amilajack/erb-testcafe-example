5685040b672d8df12a5ad90f37b0ca2e051e70a9
Amila Welihinda <amilajack@gmail.com>
2018-07-30 21:58:45 -0700

    Migrated all erb tests to testcafe

--- test/e2e/e2e.spec.js
+++
@@ -1,9 +0,0 @@
-import { ClientFunction } from 'testcafe';
-
-fixture`Electron test`.page('../../app/app.html');
-
-const getPageTitle = ClientFunction(() => document.title);
-
-test('e2e', async t => {
-  await t.expect(getPageTitle()).eql('Hello Electron React!');
-});
